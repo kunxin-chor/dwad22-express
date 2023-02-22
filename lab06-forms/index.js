6d6ef6475dc9f88f309d862f3c2bdab62311da72
kunxin-chor <chorkunxin@yahoo.com>
2023-02-22 06:10:36 +0000

    Added form example

--- lab06-forms/index.js
+++ lab06-forms/index.js
@@ -15,7 +15,51 @@ app.get("/add-food", function(req,res){
 
 app.post("/add-food", function(req,res){
     console.log(req.body);
-    res.send("form recieved");
+    // assign default values if none specified in the form (req.body)
+    const foodName = req.body.foodName || "none given";
+    const calories = req.body.calories || 0;
+    // let meal = req.body.meal;
+    // if (!meal) {
+    //     meal = "not selected";
+    // }
+
+    // const meal = req.body.meal ? req.body.meal : "not selected";
+    const meal = req.body.meal || "not selected";
+
+    // normalize req.body.tags will ALWAYS be an array
+    // if no tags selected -> empty array
+    // if one tag selected -> array of one string
+    // if two or more tags selected --> array of many strings
+
+    // most straightforward
+    let tags = [];
+
+    // if the user submitted tags is not faslely (i.e not undefined
+    // or that the person at least one)
+    if (req.body.tags) {
+        // if the user selects more than one tags
+        // Array is a global variable that all JS files can use
+        // isArray takes in one parameter and returns true if that
+        // parameter is an array
+        if (Array.isArray(req.body.tags)) {
+            tags = req.body.tags;
+        } else {
+            // otherwise, the user has only selected one tag
+            // and therefore req.body.tags is a single string
+            // so we just add it to the empty tags array
+            tags.push(req.body.tags)
+        }
+    }
+    // tags is guaranteed to be an array
+    console.log("tags =", tags);
+
+
+    res.render("submitted",{
+        "foodName": foodName,
+        "calories": calories,
+        "tags": tags,
+        "meal": meal
+    })
 })
 
 app.listen(3001, function(){
