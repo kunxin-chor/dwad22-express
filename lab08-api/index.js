f67a76d22921c157fa7717302e1d6cab805cad31
kunxin-chor <chorkunxin@yahoo.com>
2023-03-07 06:32:02 +0000

    Added new route to add in a new food-sightibg

--- lab08-api/index.js
+++ lab08-api/index.js
@@ -5,11 +5,8 @@ const express = require("express");
 // into our OS
 require('dotenv').config();
 
-// process is always available
-// it refers to current program that is running
-console.log(process.env);
-
 const app = express();
+app.use(express.json()); // enable JSON to be send via POST
 
 
 // for our Express to talk Mongo, likewise we need a client
@@ -20,27 +17,71 @@ async function main() {
     // connect to MongoDB we need two piece of information
     // first parameter - the connection string
     // second parameter - a configuration object
+
+    // process is always available
+// it refers to current program that is running
     const client = await MongoClient.connect(process.env.MONGO_URI, {
         "useUnifiedTopology": true  // simplify our access to MongoDB
     });
 
     // get the sample_airbnb database and
     // store it in the db variable
-     const db = client.db("sample_airbnb");
+    // if we try to use a database that doesn't exist
+    // mongo will assume we are creating a database
+     const db = client.db("dwad20-free-food-sightings");
 
-    app.get("/", async function(req,res){
-        // select the first 20 documents
-        // db.listingsAndReviews.find({}).limit(20)
-        const listings = await db.collection("listingsAndReviews")
-          .find({})
-          .limit(20)
-          .toArray(); // convert an array of objects
 
-          console.log(listings);
+    // app.get("/", async function(req,res){
+    //     // select the first 20 documents
+    //     // db.listingsAndReviews.find({}).limit(20)
+    //     const listings = await db.collection("listingsAndReviews")
+    //       .find({})
+    //       .limit(20)
+    //       .toArray(); // convert an array of objects
 
-          // send back to the client in the JSON format
-          res.json(listings);
+    //       console.log(listings);
+
+    //       // send back to the client in the JSON format
+    //       res.json(listings);
+    // })
+
+    // adding a new food sighting ==> adding a new food sighting resource
+    // so we use "POST"
+    // the url should only contain the resource name, no verbs or action words
+    app.post("/food-sighting", async function(req,res){
+
+        if (!req.body.title) {
+            // we have to tell the client that the title can't be
+            res.status(400);
+            res.json({
+                "error":"You must provide a title"
+            });
+            return; // end the function
+        }
+
+        try {
+            const result = await db.collection("sightings")
+                        .insertOne({
+                            "title": req.body.title,
+                            "food": req.body.food,
+                            "datetime": new Date(req.body.datetime)
+                        });
+            // send back the result so the client
+            // knows whether it is successful or not
+            // and what the ID of the new document is
+            res.json({
+            "result": result
+            });
+        }  catch (err) {
+            console.log(err);
+            res.status(503);
+            res.json({
+                "error":"Database not available. Please try later"
+            })
+        }
+       
     })
+
 
 }
 
---
+++ lab08-api/mock_data.js
@@ -0,0 +1,16 @@
+// An array to represent a collection
+// sightings collection
+[
+    {
+        "_id":ObjectId("123"),
+        "title":"Free food at LT2A",
+        "food":["curry chicken","rice","kang kong"],
+        "datetime": "2023-03-07"
+    },
+    {
+        "_id":ObjectId("124"),
+        "title":"Leftover food after seminar",
+        "food":["egg sandwich","chicken sandwich"],
+        "datetime": "2023-03-07"
+    }
+]
