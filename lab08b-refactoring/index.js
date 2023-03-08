947af7c62b2cc6d71eb8e2a3408b1673e78c62d9
kunxin-chor <chorkunxin@yahoo.com>
2023-03-08 03:11:29 +0000

    Added CR and D for comments

--- lab08b-refactoring/index.js
+++ lab08b-refactoring/index.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 // use consts instead of hard-coded strings
 const COLLECTION = "sightings";
 const DB = "dwad20-free-food-sightings";
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 app.use(express.json()); // enable JSON to be send via POST
@@ -21,53 +22,53 @@ async function main() {
     // second parameter - a configuration object
 
     // process is always available
-// it refers to current program that is running
+    // it refers to current program that is running
     const db = await MongoUtil.connect(MONGO_URI, DB);
 
 
-   
+
 
     // adding a new food sighting ==> adding a new food sighting resource
     // so we use "POST"
     // the url should only contain the resource name, no verbs or action words
-  
-    app.post("/food-sighting", async function(req,res){
+
+    app.post("/food-sighting", async function (req, res) {
 
         if (!req.body.title) {
             // we have to tell the client that the title can't be
             res.status(400);
             res.json({
-                "error":"You must provide a title"
+                "error": "You must provide a title"
             });
             return; // end the function
         }
 
         try {
             const result = await db.collection(COLLECTION)
-                        .insertOne({
-                            "title": req.body.title,
-                            "food": req.body.food,
-                            "datetime": new Date(req.body.datetime)
-                        });
+                .insertOne({
+                    "title": req.body.title,
+                    "food": req.body.food,
+                    "datetime": new Date(req.body.datetime)
+                });
             // send back the result so the client
             // knows whether it is successful or not
             // and what the ID of the new document is
             res.json({
-            "result": result
+                "result": result
             });
-        }  catch (err) {
+        } catch (err) {
             console.log(err);
             res.status(503);
             res.json({
-                "error":"Database not available. Please try later"
+                "error": "Database not available. Please try later"
             })
         }
-       
+
     })
 
-    
+
     // retrieve all existing food sighting
-    app.get("/food-sighting", async function(req,res){
+    app.get("/food-sighting", async function (req, res) {
 
         // accessing query strings: 
         // console.log(req.query);
@@ -87,32 +88,32 @@ async function main() {
                 "$in": [req.query.food]
             }
         }
-  
+
         const sightings = await db.collection(COLLECTION).find(filter).toArray();
-         
+
         res.json({
             "sightings": sightings
         })
     })
 
     // modify a document
-    app.put("/food-sighting/:food_id", async function(req,res){
-        
+    app.put("/food-sighting/:food_id", async function (req, res) {
+
         // get the ID of the document that we want to change
         const foodId = req.params.food_id;
-        
+
         // the data will in req.body
         const response = await db.collection(COLLECTION)
-                            .updateOne({
-                                "_id": new ObjectId(foodId)
-                            },{
-                                "$set": {
-                                    "title": req.body.title,
-                                    "food": req.body.food,
-                                    "datetime": new Date(req.body.datetime)
-                                }
+            .updateOne({
+                "_id": new ObjectId(foodId)
+            }, {
+                "$set": {
+                    "title": req.body.title,
+                    "food": req.body.food,
+                    "datetime": new Date(req.body.datetime)
+                }
 
-                            });
+            });
 
         res.json({
             "status": response
@@ -121,7 +122,7 @@ async function main() {
 
     })
 
-    app.delete("/food-sighting/:food_id", async function(req,res){
+    app.delete("/food-sighting/:food_id", async function (req, res) {
         const result = await db.collection(COLLECTION).deleteOne({
             "_id": new ObjectId(req.params.food_id)
         })
@@ -132,10 +133,66 @@ async function main() {
         })
     });
 
+    // begin CRUD for comments
+
+    // create a new comment for an existing food_sighting
+    // the name of the resource you are creating in the url
+    app.post("/food-sighting/:food_id/comments", async function(req,res){
+        // we are adding a new comment to the food sighting with id equals to req.params.food_id
+        const result = await db.collection(COLLECTION).updateOne({
+            "_id": new ObjectId(req.params.food_id)
+        },{
+            "$push": {
+                "comments":{
+                    "_id": new ObjectId(),
+                    "text": req.body.text,
+                    "email": req.body.email
+                }
+            }
+        });
+        res.json({
+            "result": result
+        })
+    })
+
+    // update an existing comment
+    app.put("/food-sighting/:food_id/comments/:comment_id", async function(req,res){
+        const results  = await db.collection(COLLECTION).updateOne({
+            "_id": new ObjectId(req.params.food_id),
+            "comments._id": new ObjectId(req.params.comment_id)
+        },{
+            "$set": {
+                "comments.$.text": req.body.text,
+                "comments.$.email": req.body.email
+            }
+        });
+        res.json({
+            "results": results
+        });
+    })
+
+    app.delete("/food-sighting/:food_id/comments/:comment_id", async function(req,res){
+        const results = await db.collection(COLLECTION)
+                            .updateOne({
+                                "_id": new ObjectId(req.params.food_id),
+                            },{
+                                "$pull": {
+                                    "comments":{
+                                        "_id": new ObjectId(req.params.comment_id)
+                                    }
+                                }
+                            })
+        res.json({
+            "results": results
+        })
+    })
+
+
+
 }
 
 main();
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started");
 })
--- lab08b-refactoring/mock_data.js
+++ lab08b-refactoring/mock_data.js
@@ -1,16 +1,45 @@
 // An array to represent a collection
+
+const { ObjectId } = require("mongodb");
+
 // sightings collection
 [
     {
         "_id":ObjectId("123"),
         "title":"Free food at LT2A",
         "food":["curry chicken","rice","kang kong"],
-        "datetime": "2023-03-07"
+        "datetime": "2023-03-07",
+        "comments":[
+            {
+                "_id":ObjectId("456"),
+                "text":"The curry chicken tastes sour",
+                "email":"lemon@asd.com"
+            },
+            {
+                "_id":ObjectId("456"),
+                "text":"The curry chicken tastes sour",
+                "email":"lemon@asd.com"
+            }
+
+        ]
     },
     {
         "_id":ObjectId("124"),
         "title":"Leftover food after seminar",
         "food":["egg sandwich","chicken sandwich"],
-        "datetime": "2023-03-07"
+        "datetime": "2023-03-07",
+        "comments":[ObjectId(203), ObjectId(204)]
     }
+]
+
+// references
+//COMMENTS COLLECTION
+[
+    {
+        "_id": ObjectId("7788"),
+        "sighting_id": ObjectId("124"),
+        "text":"The sandwich is from Four Leaf and tastes very good",
+        "email":"Tan Ah Kow"
+    }
+
 ]
